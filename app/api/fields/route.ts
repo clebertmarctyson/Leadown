50277746eddc169a71a91f813f1132a2d9cf0b1d
clebertmarctyson <marctysonclebert9@gmail.com>
2023-11-21 03:24:31 -0500

    Merge remote changes

--- app/api/fields/route.ts
+++ app/api/fields/route.ts
@@ -1,9 +1,7 @@
-// import { prisma } from "../auth/[...nextauth]/route";
 import { fields } from "@/data/data";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // const fields = await prisma.field.findMany();
   return NextResponse.json(fields, {
     status: 200,
   });
