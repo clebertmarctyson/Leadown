d1726cae537144f5894ddc38cb67d14a8b56302b
clebertmarctyson <marctysonclebert9@gmail.com>
2023-11-21 03:37:42 -0500

    Merge remote changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,12 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient, User } from "@prisma/client/edge";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+export const prisma = new PrismaClient();
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
@@ -12,7 +17,7 @@ const handler = NextAuth({
   callbacks: {
     session: async ({ session, user }) => {
       if (session?.user) {
-        session.user = user;
+        session.user = user as User;
       }
 
       return session;
