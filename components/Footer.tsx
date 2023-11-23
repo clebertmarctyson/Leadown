4fb8b261ad55e75405a7285ece33e8cec8a9f869
clebertmarctyson <marctysonclebert9@gmail.com>
2023-11-23 17:53:35 -0500

    Merge remote changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,16 +2,12 @@
 
 import MenuItems from "./MenuItems";
 
-import { useSession } from "next-auth/react";
-
 const Footer = () => {
-  const { data: session } = useSession();
-
   return (
     <footer className="border-t">
       <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center" aria-label="Footer">
-          <MenuItems session={session!} />
+          <MenuItems />
         </nav>
         <p className="mt-8 text-center text-base text-gray-400">
           &copy; 2023 LeadOwn. All rights reserved.
